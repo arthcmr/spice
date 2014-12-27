9cbb26a55936c5d2c8c992e0945f25e49a66e2dd
Arthur Camara <arthurcamara@gmail.com>
2014-12-27 19:43:14 -0200

    Add speech-to-text recognition

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,24 +2,55 @@ var spice_display = $("#spice_display");
 
 //the div exists
 if (spice_display.is(':visible')) {
-	spice_display.animate({right:"30px"},200).animate({right:"-330px"},300, function() {
-		spice_display.hide();
-	});
-}
-else if (spice_display.is(':hidden')) {
-	spice_display.show();
-	spice_display.animate({right:"-300px"},0).animate({right:"30px"},300).animate({right:"15px"},200);
+    spice_display.animate({
+        right: "30px"
+    }, 200).animate({
+        right: "-330px"
+    }, 300, function() {
+        spice_display.hide();
+    });
+} else if (spice_display.is(':hidden')) {
+    spice_display.show();
+    spice_display.animate({
+        right: "-300px"
+    }, 0).animate({
+        right: "30px"
+    }, 300).animate({
+        right: "15px"
+    }, 200);
+
+    listen();
 }
 
 //theres no div yet
 else {
 
-	//add css for our display
-	var stylesheet = document.createElement('link');
-	stylesheet.rel = 'stylesheet';
-	stylesheet.href = chrome.extension.getURL('../styles/main.css');
-	document.head.appendChild(stylesheet);
+    //add css for our display
+    var stylesheet = document.createElement('link');
+    stylesheet.rel = 'stylesheet';
+    stylesheet.href = chrome.extension.getURL('../styles/main.css');
+    document.head.appendChild(stylesheet);
 
-	//add markup
-	$("body").append("<div id='spice_display'>SPICYYYY!</div>");
+    //add markup
+    $("body").append("<div id='spice_display'>SPEAK NOW <div id='spice_sentence'></div> <div id='spice_history'></div></div>");
+
+    spice_display.css({
+        right: "-330px"
+    });
+    spice_display.show();
+    spice_display.animate({
+        right: "-300px"
+    }, 0).animate({
+        right: "30px"
+    }, 300).animate({
+        right: "15px"
+    }, 200);
+
+    listen();
+}
+
+function listen() {
+
+	Spice.init();
+
 }
